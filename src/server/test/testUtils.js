45552c51490ac080983b6646fb32daacdd4c8606
Philip Tchernavskij <ptchernavskij@protonmail.com>
2020-08-26 14:14:30 -0400

    FLUID-6494: Reasons long forgotten

--- src/server/test/testUtils.js
+++ src/server/test/testUtils.js
@@ -16,16 +16,3 @@ fluid.test.spreadsheets.assertStatusCode = function (request, expectedStatusCode
     var response = request.nativeResponse;
     jqUnit.assertEquals("Response has status code " + expectedStatusCode, expectedStatusCode, response.statusCode);
 };
-
-// TODO: make a GET /components/ Nexus API endpoint
-fluid.test.spreadsheets.assertComponentModel = function (request, expectedModelContent) {
-
-    // these are what I would execute in a local context
-    var that = fluid.componentForPath("path");
-
-    var value = fluid.getForComponent(that, "model.thing.thing");
-
-    // it seems like for asynchronously fetched models, we either need to delay component construction messages or get their contents specifically via model binding
-    // I suspect the latter is more appropriate, considering that there could easily be legitimate contexts where we want to let the component hang around before the model reaches its final state.
-    // the first message is the model content
-};
